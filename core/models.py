62d15ea31a9c20c1b649012e8700c318415758f1
Işık Kaplan <isik.kaplan@outlook.com>
2021-01-02 07:59:24 +0300

    Create BlogPost model and related migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='blog_posts')
+
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
